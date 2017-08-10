3dce9f40d9e0a3853f8df74c5789c3e3a1cdb39d
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-10 14:09:41 -0500

    Refactored auth and routes from index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const passport = require('passprt');
-const GoogleStrategy = require('passport-google-oauth20').GoogleStrategy;
-
+const authRoutes = require('./routes/authRoutes');
+require('./services/passport');
 
 const app = express();
-
-passport.use(new GoogleStrategy());
+authRoutes(app);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port);
---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate(
+      // This keyword is specific to GoogleStrategy
+      'google',
+      {
+        // Google has a list of specific scopes we can ask for
+        scope: ['profile', 'email']
+      }
+    )
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
