2c85fca6dec3704d2624450695e68740ff0a1e80
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-10 18:18:52 -0500

    Set up basic Mongodb URI

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI, {
+  user: keys.mongoUser,
+  password: keys.mongoPass
+}, a => {
+  console.log(a);
+});
 
 const app = express();
 authRoutes(app);
