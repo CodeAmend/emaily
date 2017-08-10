ad16108fa3137e83171c310f3ce68619a1e03c02
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-10 18:59:02 -0500

    DB saves GoogleID to users

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -11,10 +14,11 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access: ', accessToken);
-      console.log('refresh: ', refreshToken);
-      console.log('profile: ', profile);
-      console.log('done: ', done);
+      // console.log('access: ', accessToken);
+      // console.log('refresh: ', refreshToken);
+      // console.log('profile: ', profile);
+      // console.log('done: ', done);
+      new User({ googleID: profile.id }).save();
     }
   )
 );
