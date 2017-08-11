1fb1486b1c542f5a21181b7b4389e52aeafbd06d
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-10 19:58:33 -0500

    Sign in google api/current_user, shows logged in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
@@ -10,11 +12,20 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, {
   user: keys.mongoUser,
   password: keys.mongoPass
-}, a => {
-  console.log(a);
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const port = process.env.PORT || 5000;
