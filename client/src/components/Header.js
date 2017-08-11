5da61a31f5292247f53f9f8e4324b7903f8cf261
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-11 17:59:55 -0500

    Materialize CSS setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const Header = () => <div>Header</div>;
+const Header = () => {
+  return (
+     <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li><a href="/auth/google/">Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
+  )
+}
 
 export default Header;
