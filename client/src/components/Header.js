f59e70c5495a06aaf50673b484af476ef3702d2e
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-12 09:40:11 -0500

    Logout redirects to Landing page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import  { connect } from 'react-redux';
 
-const Header = () => {
-  return (
-     <nav>
+class Header extends Component {
+
+  getAuthHeader() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google/">Login with Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+
+    }
+  }
+  render() {
+    return (
+      <nav>
         <div className="nav-wrapper">
           <a href="#" className="left brand-logo">Emaily</a>
           <ul className="right">
-            <li><a href="/auth/google/">Login with Google</a></li>
+            {this.getAuthHeader()}
           </ul>
         </div>
       </nav>
-  )
+    )
+  }
 }
 
-export default Header;
+function mapDispatchToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapDispatchToProps)(Header);
