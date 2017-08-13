98926ae61d710177b51e5e7c0900b08e01c3b3e0
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-12 23:08:33 -0500

    Payment setup with react-stripe-checkout and env

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   mongoUser: process.env.MONGO_USER,
   mongoPass: process.env.MONGO_PASS,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripPublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
